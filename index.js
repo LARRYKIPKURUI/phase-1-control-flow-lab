46068cd5bca75b4d015683d63befd5d43cd52ba6
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-03-04 11:01:25 +0300

    reviewing control flow

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400) {
+    return 'This one is on me!';
+  } else if (ride > 400 && ride <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (ride > 2000 && ride <= 2500) {  // Now correctly handles 2001-2500 range
+    return 'I will gladly take your thirty bucks.';
+  } else if (ride > 2500) {
+    return 'No can do.';
+  }
+}
+console.log(scuberGreetingForFeet(199));
+console.log(scuberGreetingForFeet(1500));
+console.log(scuberGreetingForFeet(2100));
+console.log(scuberGreetingForFeet(2501));
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+console.log(ternaryCheckCity('Pittsburgh'));
+console.log(ternaryCheckCity('NYC'));
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+console.log(switchOnCharmFromTip("generous"));       
+console.log(switchOnCharmFromTip("not as generous")); 
+console.log(switchOnCharmFromTip("cheap"));          
